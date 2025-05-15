4fc8916b4f1607ec897dbe82a20e29e57d4ea331
Favour Adimora <favourdimora43@gmail.com>
2025-05-15 06:48:36 +0100

    feat: add @vercel/speed-insights integration to enhance performance monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Index from "./pages/Index";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
@@ -20,6 +21,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <Analytics />
+        <SpeedInsights />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects" element={<Projects />} />
